b17d54380c37da7dc0cd2bf3b25cfc670346cf51
p76081158 <p76081158@gs.ncku.edu.tw>
2021-06-17 00:59:44 +0800

    new feature: network slice scaleout

--- nssmf.go
+++ nssmf.go
@@ -11,13 +11,26 @@ import (
 )
 
 func GetgNBinfo() (gnb_ip string, gnb_n3_ip_B string) {
-
 	return gnb_ip, gnb_n3_ip_B
 }
 
 func DeployNetworkSlice(snssai string, gnb_ip string, gnb_n3_ip_B string, ngci string, cpu int, core_function_cpu int) {
 	arg := snssai + " " + gnb_ip + " " + gnb_n3_ip_B + " " + ngci + " " + strconv.Itoa(cpu) + " " +  strconv.Itoa(core_function_cpu)
 	slice_cmd := "shell-script/slice-deploy.sh " + arg
+	input_cmd := slice_cmd
+	cmd := exec.Command("/bin/sh", "-c", input_cmd)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Got error: %s\n", err.Error())
+	}
+	return
+}
+
+func ScaleOut(snssai string) {
+	arg := snssai
+	slice_cmd := "shell-script/slice-scaleout.sh " + arg
 	input_cmd := slice_cmd
 	cmd := exec.Command("/bin/sh", "-c", input_cmd)
 	cmd.Stdin = os.Stdin
